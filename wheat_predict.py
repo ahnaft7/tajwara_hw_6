39d8472c0394437f2700addb3f42585d7b448d21
Ahnaf Tajwar <tajwara@bu.edu>
2024-04-18 00:16:06 -0400

    added logistic regression

--- wheat_predict.py
+++ wheat_predict.py
@@ -6,7 +6,9 @@ Homework Problems # 1-3
 Description of Problem (just a 1-2 line summary!): These problems are to 
 """
 import pandas as pd
+from sklearn.calibration import LabelEncoder
 from sklearn.discriminant_analysis import StandardScaler
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -148,6 +150,63 @@ print("Accuracy:", accuracy)
 
 # Calculate confusion matrix
 conf_matrix = confusion_matrix(Y_test, Y_pred)
+
+print("Confusion Matrix: (1 is Negative, 2 is Positive)")
+print("\t\tPredicted labels")
+print("\t\t 1    2")
+print("Actual labels 1", conf_matrix[0])
+print("              2", conf_matrix[1])
+
+tp = conf_matrix[1][1]
+fn = conf_matrix[1][0]
+fp = conf_matrix[0][1]
+tn = conf_matrix[0][0]
+print("TP: ", tp)
+print("FP: ", fp)
+print("TN: ", tn)
+print("FN: ", fn)
+
+# Calculate True Positive Rate
+tpr = tp / (tp + fn)
+
+# Calculate True Negative Rate
+tnr = tn / (tn + fp)
+
+print("True Positive Rate:", tpr)
+print("True Negative Rate:", tnr)
+
+#---------------------Logistic Regression-------------------
+
+print("\n-------------Logistic Regression--------------")
+
+# Test Train split for filtered_df
+features = ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7']
+class_labels = [1, 2]
+
+X = filtered_df[features].values
+
+le = LabelEncoder()
+Y = le.fit_transform(filtered_df['L'].values)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.5, random_state=1)
+# Y_train = np.ravel(Y_train)
+# Y_test = np.ravel(Y_test)
+
+# Initialize the logistic regression model
+logistic_regression = LogisticRegression()
+
+# Train the model on the training data
+logistic_regression.fit(X_train, Y_train)
+
+# Predict labels for the test set
+y_pred = logistic_regression.predict(X_test)
+
+# Calculate accuracy
+accuracy = accuracy_score(Y_test, y_pred)
+print("Accuracy:", accuracy)
+
+# Calculate confusion matrix
+conf_matrix = confusion_matrix(Y_test, y_pred)
 
 print("Confusion Matrix: (1 is Negative, 2 is Positive)")
 print("\t\tPredicted labels")
